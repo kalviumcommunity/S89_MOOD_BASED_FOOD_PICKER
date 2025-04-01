fe35f8717c3293eadee5ac990bab8cef9d767de7
rohithagannamaneedi <rohithagannamaneedi@gmail.com>
2025-04-01 16:12:14 +0530

    test

---
+++ frontend/src/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1> 🥨🍔🍟 Mood Based Food Picker 🍕🍜🧁</h1>
+        <p style ={{fontSize:"large"}}>Let your mood decide what you eat in the day</p>
+        <button style={{
+          padding: "10px 20px",
+          backgroundColor: "light blue",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+      }}>
+            Get Started
+        </button>
+    </div>
+  )
+}
+
+export default About
+
